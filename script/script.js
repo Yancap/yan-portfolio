889dd1883e02a7f096556f29d4551d98df601a2e
Yancap <yangabriel3001f@gmail.com>
2023-01-13 13:47:09 -0300

    "first animation"

--- script/script.js
+++ script/script.js
@@ -18,3 +18,40 @@ btn.addEventListener("click", function(busca){
 })
 
 
+
+
+function showSectionScrolled(){
+    const sections = document.querySelectorAll(".scroll")
+    sections.forEach((section)=>{
+        if (!section.classList.value.includes("active-scroll")) {
+          
+          const topSection = section.getBoundingClientRect().top - (window.innerHeight * 0.6)
+          if(topSection < 0){
+            if(section == document.getElementById("capacidades")){
+              section.classList.add('active-scroll')
+              let cards = document.querySelectorAll(".accordion")
+              document.querySelector("#capacidades header").style.animation = "show-title 1s forwards"
+              
+              let i = 1
+              cards.forEach((card)=>{
+
+                  card.style.animation = "skill-content 1s " + i*0.2 +"s forwards"
+                  card.style.animationDelay
+                  console.log("a");
+                  i++
+                
+              })
+            }
+            if(section == document.getElementById("soft-skills")){
+              
+            }
+          }
+        }else{
+          
+        }
+        
+
+    })
+}
+
+window.addEventListener('scroll', showSectionScrolled)
